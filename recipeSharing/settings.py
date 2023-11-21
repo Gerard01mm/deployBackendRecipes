135ddd671bdbc904e37999bf41749902043ceb3c
Gerard01mm <56315258+Gerard01mm@users.noreply.github.com>
2023-11-21 16:19:54 +0100

    settings upadted with environ variables

--- recipeSharing/settings.py
+++ recipeSharing/settings.py
@@ -1,5 +1,6 @@
 # Import the 'Path' class from the 'pathlib' module
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,13 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$p9@j@+pi_@f!19nv4lv(a=hnmz70qto1z*-7820*z*dpe^_q4'
+SECRET_KEY = os.environ.get("SECRET_KEY")#'django-insecure-$p9@j@+pi_@f!19nv4lv(a=hnmz70qto1z*-7820*z*dpe^_q4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # List of allowed hostnames or IP addresses for this project
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")#[]
 
 # Application definition
 INSTALLED_APPS = [
