75b169ea4f4f948b72a92691b35bcf36c7b52a12
Gerard01mm <56315258+Gerard01mm@users.noreply.github.com>
2023-11-21 21:25:50 +0100

    Backend with cors v1

--- recipeSharing/settings.py
+++ recipeSharing/settings.py
@@ -25,19 +25,25 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.apps.AppConfig',  # Define the app for this project
+    'app.apps.AppConfig',
+    'corsheaders'# Define the app for this project
 ]
 
 # Middleware settings
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+# Middleware para cors
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = "*"
 
 # URL for redirecting after a successful login
 LOGIN_REDIRECT_URL = "/"
